660e12a97888902d2366b8465c9b862adf1ba5e2
Tejas <tejusamant19@gmail.com>
2022-11-05 21:51:54 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './MyComponents/Navbar';
+import { Heading } from './MyComponents/Heading';
+import { Courses } from './MyComponents/Courses';
+import { Slides } from './MyComponents/Slides';
+import { Contact } from './MyComponents/Contact';
+import { About } from './MyComponents/About';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+      <Heading />
+      <Navbar />
+        <Routes>
+          <Route path="/about" element={<About Greet="Mr.Nikam" Principal="Mr.Choughle" HOD="Mr.Samant" Chairman="Mr.Pal"/>}/>
+          <Route path="/" element={[<Courses />,<Slides />]}/>
+          <Route path="/home" element={[<Courses />,<Slides />]}/>
+        </Routes>
+        <Contact /> 
+      </Router>
+    </>
   );
 }
 
